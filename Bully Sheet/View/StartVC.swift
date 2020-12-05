b46c99c177c112dd3e10752fcfabe723102d4ac8
varadzin <varadzin@gmail.com>
2020-12-05 09:22:36 +0100

    configure StartVC

--- Bully Sheet/View/StartVC.swift
+++ Bully Sheet/View/StartVC.swift
@@ -9,6 +9,39 @@ import UIKit
 
 class StartVC: UIViewController {
     
+    var padding: CGFloat = 20
+    var bookImageBtn = UIButton()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        
+    }
+    
+    
+    
+    func configBookImgBtn() {
+        view.addSubview(bookImageBtn)
+        let btnImg = UIImage(named: "book")
+        
+        bookImageBtn.setImage(btnImg, for: .normal)
+        
+        bookImageBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            bookImageBtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+            bookImageBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            bookImageBtn.widthAnchor.constraint(equalToConstant: 160),
+            bookImageBtn.heightAnchor.constraint(equalToConstant: 160)
+            
+            
+        ])
+        
+        
+    }
+    
     
     
 }

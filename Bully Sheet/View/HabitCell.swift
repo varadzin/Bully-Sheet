7e4c5ba1ac016757296b2a25a16e9e492b0bfbcc
varadzin <varadzin@gmail.com>
2021-01-10 14:53:15 +0100

    UITable Cell

---
+++ Bully Sheet/View/HabitCell.swift
@@ -0,0 +1,22 @@
+//
+//  HabitCell.swift
+//  Bully Sheet
+//
+//  Created by Frantisek Varadzin on 10/01/2021.
+//
+
+import UIKit
+
+class HabitCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+
+}
--- Bully Sheet/View/TableView.swift
+++ Bully Sheet/View/TableView.swift
@@ -10,7 +10,7 @@ import UIKit
 class TableView: UIViewController, UITableViewDelegate {
     
 
-
+            
 var myTableView = UITableView()
 
     override func viewDidLoad() {
